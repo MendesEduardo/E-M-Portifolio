0ac417660417abcf0431ec09abb66ffdf96e5615
Mendes Eduardo <eduardomn2018@gmail.com>
2023-02-24 17:14:35 -0300

    1.1

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import About from './pages/About';
 import Servicos from './pages/Servicos';
@@ -8,16 +8,18 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/sobre" element={<About />}></Route>
-        <Route path="/servicos" element={<Servicos />}></Route>
-        <Route path="/projetos" element={<Projects />}></Route>
-        <Route path="/contato" element={<Contact />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/sobre" element={<About />}></Route>
+          <Route path="/servicos" element={<Servicos />}></Route>
+          <Route path="/projetos" element={<Projects />}></Route>
+          <Route path="/contato" element={<Contact />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
