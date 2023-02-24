b9f5327d5ebacb43fdab47709a1a27514bee0e55
Mendes Eduardo <eduardomn2018@gmail.com>
2023-02-24 19:02:13 -0300

    1.4

--- src/components/MenuLink/index.jsx
+++ src/components/MenuLink/index.jsx
@@ -1,4 +1,5 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './MenuLink.scss';
 
 function MenuLink({ children, to }) {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './style/index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
